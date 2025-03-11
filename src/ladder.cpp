35859906a3b2651e06100aab198b7ae471698d37
ericcheng8 <ec8ec.er@gmail.com>
2025-03-11 04:42:42 +0000

    first try

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -94,10 +94,13 @@ void load_words(set<string> & word_list, const string& file_name) {
 
 void print_word_ladder(const vector<string>& ladder) {
     if (!ladder.empty()) {
+        cout << "Word ladder found: ";
         for (size_t i = 0; i < ladder.size(); ++i) {
             cout << ladder[i] << " ";
         }
         cout << endl;
+    } else {
+        cout << "No word ladder found.\n";
     }
 }
 
